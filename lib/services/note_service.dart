82c4c7802deabd0183c9d8656a3e6fae95256eb7
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-12-14 22:43:24 +0530

    changes

--- lib/services/note_service.dart
+++ lib/services/note_service.dart
@@ -1,28 +1,35 @@
+import 'dart:convert';
+
+import 'package:rest_api/models/api_response.dart';
 import 'package:rest_api/models/note_for_list.dart';
+import 'package:http/http.dart' as http;
 
 class NoteService {
   //
+  static const API = 'http://api.notes.programmingaddict.com';
+  static const headers = {'apiKey': '08d771e2-7c49-1789-0eaa-32aff09f1471'};
 
-  List<NoteForListing> getNoteList() {
-    return [
-      NoteForListing(
-        noteId: '1',
-        noteTitle: 'Note1',
-        createDateTime: DateTime.now(),
-        latestEditDateTime: DateTime.now(),
-      ),
-      NoteForListing(
-        noteId: '2',
-        noteTitle: 'Note2',
-        createDateTime: DateTime.now(),
-        latestEditDateTime: DateTime.now(),
-      ),
-      NoteForListing(
-        noteId: '3',
-        noteTitle: 'Note3',
-        createDateTime: DateTime.now(),
-        latestEditDateTime: DateTime.now(),
-      ),
-    ];
+  Future<ApiResponse<List<NoteForListing>>> getNotesList() {
+    return http.get(('$API/notes') as Uri, headers: headers).then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        final notes = <NoteForListing>[];
+        for (var item in jsonData) {
+          final note = NoteForListing(
+            noteId: item['noteID'],
+            noteTitle: item['noteTitle'],
+            createDateTime: DateTime.parse(item['createDateTime']),
+            latestEditDateTime: item['latestEditDateTime'] != null
+                ? DateTime.parse(item['latestEditDateTime'])
+                : null,
+          );
+          notes.add(note);
+        }
+        return ApiResponse<List<NoteForListing>>(data: notes);
+      }
+      return ApiResponse<List<NoteForListing>>(
+          error: true, errorMessage: 'An error occured');
+    }).catchError((_) => ApiResponse<List<NoteForListing>>(
+        error: true, errorMessage: 'An error occured'));
   }
 }
