001834b9a423cea25cf365de1b9b4fc6fd589fd4
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-06-21 23:37:56 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:rest_api/screens/note_list_screen.dart';
-import 'package:rest_api/services/note_service.dart';
-
-void setupLocator() {
-  GetIt.I.registerLazySingleton(() => NoteService());
-}
 
 void main() {
-  setupLocator();
   runApp(const MyApp());
 }
 
@@ -19,8 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Api Crud Operation',
-      home: NoteListScreen(),
+      title: 'Rest Api',
     );
   }
 }
