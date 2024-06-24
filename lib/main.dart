30656635cd489dd5c6f992b6a7c365607ddf0357
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-06-24 23:42:01 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rest_api/basicMethod/src/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Rest Api',
+      home: HomeScreen(),
     );
   }
 }
