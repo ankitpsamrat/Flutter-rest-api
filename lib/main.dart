b83c3bbc4294042f23711cc28d58693a6086a593
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-07-10 23:43:40 +0530

    package added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rest_api/basicMethod/src/screens/home_screen.dart';
+import 'package:rest_api/usingBloc/features/posts/ui/post_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Rest Api',
-      home: HomeScreen(),
+      home: PostPage(),
     );
   }
 }
